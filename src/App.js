e685f3460b590ead78fbbea4ad491e60f92ae437
Farah Krecinic <farahkrecinic@gmail.com>
2021-05-16 14:42:35 +0200

    added routing for multiple pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import React from 'react';
 import './App.css';
+
+import { Switch, Route } from 'react-router-dom';
+
+import Home from './pages/home/home.component';
+import Runs from './pages/runs/runs.component';
+import { GlobalStyle } from './global.styles';
 
 function App() {
   return (
     <div >
-     
+      <GlobalStyle />
+      <Switch>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/runs' component={Runs} />
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/runs/runs.component.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Runs = () => (
+    <div>Run!</div>
+);
+
+export default Runs;
