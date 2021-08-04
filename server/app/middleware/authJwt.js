e64564f857c795a3aed1a61e92ade33d249dd9b8
Farah Desktop <farahkrecinic@gmail.com>
2021-08-04 12:10:09 +0200

    just checking again

--- server/app/middleware/authJwt.js
+++ server/app/middleware/authJwt.js
@@ -1,6 +1,6 @@
 //author: bezkoder
 //https://www.bezkoder.com/node-js-mongodb-auth-jwt/
-//just checking
+//just checking again
 
 const jwt = require("jsonwebtoken");
 const config = require("../config/auth.config.js");
